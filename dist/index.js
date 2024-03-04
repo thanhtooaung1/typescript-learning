e3ab02b866f3937611733aa4538d7ad1cb62a49d
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-03-04 14:55:46 +0630

    array finished

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,9 @@
 "use strict";
-let person = "Mg Mg";
-let age = 20;
-let isActive = true;
-let names = ["Kyaw Kyaw", "Mg Mg"];
+let names = ['Kyaw Kyaw', 'Mg Mg'];
 // names.push(true);   // type error
-names.push("Aye Aye");
-console.log(names);
+names.push('Aye Aye');
+console.log(names[0]);
 let ages = [22, 23];
 // ages.push('30');   // type error
 ages.push(20);
-console.log(ages);
-let student = {
-  name: "Mg Mg",
-  age: 22,
-  male: true,
-};
-// student.name = 20; // type error
-student.age = 23;
-console.log(student);
-let father;
-let mother;
-father = "U Ba"; // type error
-mother = "Daw Mya"; /// type error
+console.log(ages[1]);
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,9 @@
-let person: string = 'Mg Mg';
-let age: number = 20;
-let isActive: boolean = true;
-
-let names = ['Kyaw Kyaw', 'Mg Mg'];
+let names: string[] = ['Kyaw Kyaw', 'Mg Mg'];
 // names.push(true);   // type error
 names.push('Aye Aye');
-console.log(names);
+console.log(names[0]);
 
 let ages = [22, 23];
 // ages.push('30');   // type error
 ages.push(20);
-console.log(ages);
-
-let student = {
-    name: 'Mg Mg',
-    age: 22,
-    male: true
-};
-// student.name = 20; // type error
-student.age = 23;
-console.log(student);
-
-
-let father:null;
-let mother: undefined;
-
-father = "U Ba"; // type error
-
-mother = "Daw Mya"; // type error
+console.log(ages[1]);
