32be845414c59f589f78d006fb81ef6546676d12
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-03-04 14:02:29 +0630

    tsc config

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,24 @@
 "use strict";
-let person = 'Mg Mg';
+let person = "Mg Mg";
 let age = 20;
 let isActive = true;
-let names = ['Kyaw Kyaw', 'Mg Mg'];
+let names = ["Kyaw Kyaw", "Mg Mg"];
 // names.push(true);   // type error
-names.push('Aye Aye');
+names.push("Aye Aye");
 console.log(names);
 let ages = [22, 23];
 // ages.push('30');   // type error
 ages.push(20);
 console.log(ages);
 let student = {
-    name: 'Mg Mg',
-    age: 22,
-    male: true
+  name: "Mg Mg",
+  age: 22,
+  male: true,
 };
 // student.name = 20; // type error
 student.age = 23;
 console.log(student);
+let father;
+let mother;
+father = "U Ba"; // type error
+mother = "Daw Mya"; /// type error
