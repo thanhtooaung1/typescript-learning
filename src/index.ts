a1c64b2ab1df6d72c4c0590b868d3681eefb8ce4
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-03-04 15:30:37 +0630

    any data type finished

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-function addTwoNumbers(num1: number, num2: number): number {
-    return num1 + num2;
+let container: any
+
+// dynamic values
+container = "Mg Mg";
+container = true;
+container = 20;
+
+// any types parameter and return any value
+function doSomething(value1: any, value2: any):any {
+    return value1 + value2;
 }
 
-function substractTwoNumbers(num1:number, num2: number):number {
-    return num1 - num2;
-}
+const result = doSomething("hello", 20);
 
-function addAllNumbers(numbers: number[]):number {
-    return numbers.reduce((t, c) => t+c, 0);
-}
-
-// const result = addTwoNumbers(5,8);
-// const result = substractTwoNumbers(20, 5);
-const result = addAllNumbers([20, 5, 6]);
-
-console.log(result);
+// any type array
+const array: any[] = ["Hello", 30, true];
