5f684c7c12090cd93e3d65293bc5b79c037ce146
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-03-04 13:45:29 +0630

    variable types (number, string, boolean, null, undified, object) finished

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+let person = 'Mg Mg';
+let age = 20;
+let isActive = true;
+let names = ['Kyaw Kyaw', 'Mg Mg'];
+// names.push(true);   // type error
+names.push('Aye Aye');
+console.log(names);
+let ages = [22, 23];
+// ages.push('30');   // type error
+ages.push(20);
+console.log(ages);
+let student = {
+    name: 'Mg Mg',
+    age: 22,
+    male: true
+};
+// student.name = 20; // type error
+student.age = 23;
+console.log(student);
